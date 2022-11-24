a5f35a781de6c2db762c44c6c1eaf8d166a2aad1
EliaBrambilla <elia.brambilla@icloud.com>
2022-11-24 01:22:59 +0100

    aggiungo card component vue

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="cards-container">
+      <div class="card">
+         <div><img :src="data.thumb" alt=""></div>
+         <div><p>{{data.series}}</p></div>
+         <div><p>{{data.type}}</p></div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+      name: 'CardComponent',
+  props:{
+      data: Object,
+  }
+  }
+  </script>
+  
+  <style scoped>
+      img{
+          width: 100px;
+      }
+  .cards-container{
+          display: flex;
+          align-items: center;
+          flex-wrap: wrap;
+          max-width: 1200px;
+          margin: 0 auto;
+      }
+  .card{
+      color: white;
+      width: calc(100% / 4);
+      height: 150px;
+      background-color: bisque;
+      padding: 15px;
+      display: flex;
+      flex-direction: column;
+  }
+  </style>
